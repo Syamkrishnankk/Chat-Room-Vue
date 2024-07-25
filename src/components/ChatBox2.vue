f8268069078100e515e7395a51a1bad08d41984c
Syam krishnan k k <86349818+Syamkrishnankk@users.noreply.github.com>
2024-07-25 09:47:00 +0530

    Update ChatBox2.vue

--- src/components/ChatBox2.vue
+++ src/components/ChatBox2.vue
@@ -1,12 +1,13 @@
 <template>
   <v-row align="center" justify="center" class="ma-5">
-    <v-card width="500" height="300" class="pa-10 overflow-auto messagelist" >
+    <v-card width="800" height="400" class="py-10 pl-2 overflow-auto messagelist" >
         <span v-for="msg in messages" :key="msg.code" >
             <span v-for="message in msg.messages" :key="msg.sender" v-if="msg.code == user" >
         <v-list two-line  >
-      <v-list-item >
 
-          <v-card color="teal-lighten-5" class="px-4 py-3" :style="[ username === message.sender ? {'float': 'right','width' : 'fit-content'} : {'float': 'left','width' : 'fit-content'}]">
+
+          <v-card color="teal-lighten-5" class="px-4 py-3 mx-2" :style="[ username === message.sender ? {'float': 'right','width' : 'fit-content'} : {'float': 'left','width' : 'fit-content'}]">
+            <v-list-item >
             <v-row class="ma-1">
     
     <v-list-item-subtitle style="font-size: 14px;" class="mb-2 text-wrap">{{ message.sender }}</v-list-item-subtitle>
@@ -16,9 +17,10 @@
 
 </v-row>
 <v-list-item-title style="font-size: 16px;" class="mb-1 text-wrap">{{ message.content }}</v-list-item-title>
-          </v-card>
+</v-list-item>         
+</v-card>
         
-      </v-list-item>
+
     </v-list>
 </span>
 </span>
